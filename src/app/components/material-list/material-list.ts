8c6e906fad57fdc76d86a561e0bad816979e8655
xavier <xavier,cortada@yahoo.es>
2016-06-30 00:10:17 +0200

    Materiales

--- src/app/components/material-list/material-list.ts
+++ src/app/components/material-list/material-list.ts
@@ -1,12 +1,9 @@
 /**
  * Created by xavier on 9/06/16.
  */
-import {Component, OnInit} from "@angular/core";
+import {Component, OnInit, Input} from "@angular/core";
 import {PartidaService} from "../../services/PartidaService";
 import {Router, ROUTER_DIRECTIVES} from '@angular/router-deprecated';
-import {ProveedorDetail} from "../proveedor-detail/proveedor-detail";
-import {PartidaModel} from "../../models/partida-model";
-import {Configuration} from "../../app.constants";
 import {MaterialModel} from "../../models/material-model";
 
 @Component({
@@ -16,27 +13,21 @@ import {MaterialModel} from "../../models/material-model";
 
 })
 export class MaterialList implements OnInit {
-  public partidas :MaterialModel[];
+  @Input() materiales :MaterialModel[];
+
+
   constructor(private _router: Router, public _service : PartidaService){
 
   }
 
   ngOnInit(){
-    this.GetAllItems();
+    
   }
 
-  onSelect(item :PartidaModel){
-    let link = ['PartidaDetail', { id: item.id }];
+  onSelect(item :MaterialModel){
+    let link = ['MatrerialDetail', { id: item.id }];
     this._router.navigate(link);
   }
 
-
-
-  private GetAllItems() :void{
-    this._service
-      .GetAll()
-      .subscribe((data :any) => this.partidas = data,
-        error => console.log(error),
-        () => console.log('Get all items complete'));
-  }
+  
 }
--- src/app/models/partida-model.ts
+++ src/app/models/partida-model.ts
@@ -4,6 +4,7 @@
 import {ProveedorModel} from "./proveedor-model";
 import {TransporteModel} from "./transporte-model";
 import {PaisModel} from "./pais-model";
+import {MaterialModel} from "./material-model";
 
 export class PartidaModel{
   id :number;
@@ -17,6 +18,8 @@ export class PartidaModel{
   proveedor :ProveedorModel;
   transporte :TransporteModel;
   pais :PaisModel;
+  
+  materiales :MaterialModel[];
 
 
   
